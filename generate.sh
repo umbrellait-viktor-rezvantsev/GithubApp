4bcb4068c04746070755ff3efa3206d382bc68fa
Viktor Rezvantsev <viktor.rezvantsev@umbrellait.com>
2023-05-03 11:50:57 +0300

    Исправил сборку

--- generate.sh
+++ generate.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 cd Configs/XcodeGen
 xcodegen
-mv -v /Users/viktorrezvantsev/Desktop/MyProjects/Layouts/GithubApp/Configs/XcodeGen/GithubApp.xcodeproj /Users/viktorrezvantsev/Desktop/MyProjects/Layouts/GithubApp/GithubApp.xcodeproj
+#mv -v /Users/viktorrezvantsev/Desktop/MyProjects/Layouts/GithubApp/Configs/XcodeGen/GithubApp.xcodeproj /Users/viktorrezvantsev/Desktop/MyProjects/Layouts/GithubApp/GithubApp.xcodeproj
 #cd ../Cocoapods
 pod install
+read -p "Open project? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    open GithubApp.xcworkspace
+fi
